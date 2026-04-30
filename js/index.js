bab23ad63b26ca0ac646d12cc43b580e8b6e3027
anissabelle <absollman42@tntech.edu>
2026-04-30 17:58:22 -0500

    Finished resume form validation

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ document.querySelector('#navSignUp').addEventListener("click", function(){
  * 
  * 
  * Showing/Hiding Resume Form Steps
+ * Validation of form elements
  * 
  * 
 ****************/ 
@@ -48,99 +49,436 @@ document.querySelector('#btnBack1').addEventListener("click", function(){
     document.querySelector('#divResume').classList.add("d-none")
     document.querySelector('#divMainPage').classList.remove("d-none")
 })
-// Resume Page: Work Experience
-document.querySelector('#btnNextStep1').addEventListener("click", function(){
-    document.querySelector('#divFirstStep').classList.add("d-none")
-    document.querySelector('#divSecondStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack2').addEventListener("click", function(){
-    document.querySelector('#divSecondStep').classList.add("d-none")
-    document.querySelector('#divFirstStep').classList.remove("d-none")
-})
-// Resume Page: Skills
-document.querySelector('#btnNextStep2').addEventListener("click", function(){
-    document.querySelector('#divSecondStep').classList.add("d-none")
-    document.querySelector('#divThirdStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack3').addEventListener("click", function(){
-    document.querySelector('#divThirdStep').classList.add("d-none")
-    document.querySelector('#divSecondStep').classList.remove("d-none")
-})
-// Resume Page: Certifications
-document.querySelector('#btnNextStep3').addEventListener("click", function(){
-    document.querySelector('#divThirdStep').classList.add("d-none")
-    document.querySelector('#divFourthStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack4').addEventListener("click", function(){
-    document.querySelector('#divFourthStep').classList.add("d-none")
-    document.querySelector('#divThirdStep').classList.remove("d-none")
-})
-// Resume Page: Awards
-document.querySelector('#btnNextStep4').addEventListener("click", function(){
-    document.querySelector('#divFourthStep').classList.add("d-none")
-    document.querySelector('#divFifthStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack5').addEventListener("click", function(){
-    document.querySelector('#divFifthStep').classList.add("d-none")
-    document.querySelector('#divFourthStep').classList.remove("d-none")
-})
-// Resume Page: Projects
-document.querySelector('#btnNextStep5').addEventListener("click", function(){
-    document.querySelector('#divFifthStep').classList.add("d-none")
-    document.querySelector('#divSixthStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack6').addEventListener("click", function(){
-    document.querySelector('#divSixthStep').classList.add("d-none")
-    document.querySelector('#divFifthStep').classList.remove("d-none")
-})
-// Resume Page: Submit Resume
-document.querySelector('#btnNextStep6').addEventListener("click", function(){
-    document.querySelector('#divSixthStep').classList.add("d-none")
-    document.querySelector('#divSeventhStep').classList.remove("d-none")
-})
-document.querySelector('#btnBack7').addEventListener("click", function(){
-    document.querySelector('#divSeventhStep').classList.add("d-none")
-    document.querySelector('#divSixthStep').classList.remove("d-none")
-})
-
-/**************** 
- * 
- * 
- * Resume Form Validation
- * Personal information is required
- * To add another item for certifications, awards, projects, etc, user must input something prior
- * Using Sweet Alert pop-ups to notify user of required fields
- * 
- * 
-****************/
 document.querySelector('#btnNextStep1').addEventListener("click", function(){
     const firstName = document.querySelector('#inputFirstName').value.trim()
     const lastName = document.querySelector('#inputLastName').value.trim()
     const email = document.querySelector('#inputEmail').value.trim()
     const phoneNo = document.querySelector('#inputPhone').value.trim()
 
+    // Used ChatGPT to help me remember how to use regex to validate email/phone
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const regexPhone = /^(\+1\s?)?(\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$/
+
     let strErrorMessage = ""
     let blnError = false
 
     if(!firstName){
-        strErrorMessage += "<p>You must enter a first name.</p>"
+        strErrorMessage += "You must enter a first name.<br>"
         blnError = true
     }
     if(!lastName){
-        strErrorMessage += "<p>You must enter a last name.</p>"
+        strErrorMessage += "You must enter a last name.<br>"
         blnError = true
     }
-    if(!email){
-        strErrorMessage += "<p>You must enter a valid email.</p>"
+    if(!email || !regexEmail.test(email)){
+        strErrorMessage += "You must enter a valid email.<br>"
         blnError = true
     }
-    if(!phoneNo){
-        strErrorMessage += "<p>You must enter a valid phone number.</p>"
+    if(!phoneNo || !regexPhone.test(phoneNo)){
+        strErrorMessage += "You must enter a valid phone number."
         blnError = true
     }
 
     if(blnError){
-
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
     }
-    
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Personal Information Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        // If all form elements are filled then user can move onto next step
+        document.querySelector('#divFirstStep').classList.add("d-none")
+        document.querySelector('#divSecondStep').classList.remove("d-none")
+    }
+})
+// Resume Page: Work Experience
+document.querySelector('#btnBack2').addEventListener("click", function(){
+    document.querySelector('#divSecondStep').classList.add("d-none")
+    document.querySelector('#divFirstStep').classList.remove("d-none")
+})
+document.querySelector('#btnAddJob').addEventListener("click", function(){
+    const jobTitle = document.querySelector('#inputJobTitle').value
+
+    strErrorMessage = "You must enter a job title to add a new job."
+
+    if(!jobTitle){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Job Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        // Clear input fields
+        // https://www.w3schools.com/howto/howto_html_clear_input.asp
+        document.querySelector('#inputJobTitle').value = ""
+        document.querySelector('#inputDescription').value = ""
+        document.querySelector('#inputStartDateJob').value = ""
+        document.querySelector('#inputEndDateJob').value = ""
+    }
+})
+document.querySelector('#btnNextStep2').addEventListener("click", function(){
+    const jobTitle = document.querySelector('#inputJobTitle').value
+    if(jobTitle){
+        let timerInterval;
+        Swal.fire({
+        title: "Job Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+    }
+
+    document.querySelector('#inputJobTitle').value = ""
+    document.querySelector('#inputDescription').value = ""
+    document.querySelector('#inputStartDateJob').value = ""
+    document.querySelector('#inputEndDateJob').value = ""
+
+    document.querySelector('#divSecondStep').classList.add("d-none")
+    document.querySelector('#divThirdStep').classList.remove("d-none")
+})
+// Resume Page: Skills
+document.querySelector('#btnBack3').addEventListener("click", function(){
+    document.querySelector('#divThirdStep').classList.add("d-none")
+    document.querySelector('#divSecondStep').classList.remove("d-none")
+})
+document.querySelector('#btnAddSkill').addEventListener("click", function(){
+    const skill = document.querySelector('#inputSkill').value
+
+    strErrorMessage = "You must enter a skill to add a new skill."
+
+    if(!skill){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Skill Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        // Clear input fields
+        // https://www.w3schools.com/howto/howto_html_clear_input.asp
+        document.querySelector('#inputSkill').value = ""
+    }
+})
+document.querySelector('#btnNextStep3').addEventListener("click", function(){
+    const skill = document.querySelector('#inputSkill').value
+    if(skill){
+        let timerInterval;
+        Swal.fire({
+        title: "Skill Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+    }
+    document.querySelector('#inputSkill').value = ""
+
+    document.querySelector('#divThirdStep').classList.add("d-none")
+    document.querySelector('#divFourthStep').classList.remove("d-none")
+})
+// Resume Page: Certifications
+document.querySelector('#btnBack4').addEventListener("click", function(){
+    document.querySelector('#divFourthStep').classList.add("d-none")
+    document.querySelector('#divThirdStep').classList.remove("d-none")
+})
+document.querySelector('#btnAddCertification').addEventListener("click", function(){
+    const certification = document.querySelector('#inputCertification').value
+
+    strErrorMessage = "You must enter a certification to add a new certification."
+
+    if(!certification){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Certification Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        document.querySelector('#inputCertification').value = ""
+        document.querySelector('#inputDateCertification').value = ""
+    }
+})
+document.querySelector('#btnNextStep4').addEventListener("click", function(){
+    const certification = document.querySelector('#inputCertification').value
+    if(certification){
+        let timerInterval;
+        Swal.fire({
+        title: "Certification Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+    }
+    document.querySelector('#inputCertification').value = ""
+    document.querySelector('#inputDateCertification').value = ""
+
+    document.querySelector('#divFourthStep').classList.add("d-none")
+    document.querySelector('#divFifthStep').classList.remove("d-none")
+})
+// Resume Page: Awards
+document.querySelector('#btnBack5').addEventListener("click", function(){
+    document.querySelector('#divFifthStep').classList.add("d-none")
+    document.querySelector('#divFourthStep').classList.remove("d-none")
+})
+document.querySelector('#btnAddAward').addEventListener("click", function(){
+    const award = document.querySelector('#inputAward').value
+
+    strErrorMessage = "You must enter an award to add a new award."
+
+    if(!award){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Award Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        document.querySelector('#inputAward').value = ""
+        document.querySelector('#inputDateAward').value = ""
+    }
+})
+document.querySelector('#btnNextStep5').addEventListener("click", function(){
+    const award = document.querySelector('#inputAward').value
+    if(award){
+        let timerInterval;
+        Swal.fire({
+        title: "Award Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+    }
+    document.querySelector('#inputAward').value = ""
+    document.querySelector('#inputDateAward').value = ""
+
+    document.querySelector('#divFifthStep').classList.add("d-none")
+    document.querySelector('#divSixthStep').classList.remove("d-none")
+})
+// Resume Page: Projects
+document.querySelector('#btnBack6').addEventListener("click", function(){
+    document.querySelector('#divSixthStep').classList.add("d-none")
+    document.querySelector('#divFifthStep').classList.remove("d-none")
+})
+document.querySelector('#btnAddProject').addEventListener("click", function(){
+    const project = document.querySelector('#inputProject').value
+
+    strErrorMessage = "You must enter a project to add a new project."
+
+    if(!project){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Project Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        document.querySelector('#inputProject').value = ""
+        document.querySelector('#inputProjectDescription').value = ""
+        document.querySelector('#inputStartDateProject').value = ""
+        document.querySelector('#inputEndDateProject').value = ""
+    }
+})
+document.querySelector('#btnNextStep6').addEventListener("click", function(){
+    const project = document.querySelector('#inputProject').value
+    if(project){
+        let timerInterval;
+        Swal.fire({
+        title: "Project Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+    }
+    document.querySelector('#inputProject').value = ""
+    document.querySelector('#inputProjectDescription').value = ""
+    document.querySelector('#inputStartDateProject').value = ""
+    document.querySelector('#inputEndDateProject').value = ""
+
+    document.querySelector('#divSixthStep').classList.add("d-none")
+    document.querySelector('#divSeventhStep').classList.remove("d-none")
+})
+// Resume Page: Submit Resume
+document.querySelector('#btnBack7').addEventListener("click", function(){
+    document.querySelector('#divSeventhStep').classList.add("d-none")
+    document.querySelector('#divSixthStep').classList.remove("d-none")
+})
+document.querySelector('#btnSubmit').addEventListener("click", function(){
+    const title = document.querySelector('#inputTitle').value.trim()
+    strErrorMessage = "You must enter a title to create resume"
+
+    if(!title){
+        Swal.fire({
+            title: "Uh Oh...",
+            html: `<p>${strErrorMessage}</p>`,
+            icon: "error"
+        });
+    }
+    else{
+        let timerInterval;
+        Swal.fire({
+        title: "Resume Submitted!",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        })
+
+        document.querySelector('#inputTitle').value = ""
+    }
 })
